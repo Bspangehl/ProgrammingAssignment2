9be89bf169efb3ba50a3ca068bc3ded1ca8a3b48
BSpangehl <bspangehl@gmail.com>
2014-04-27 17:14:11 +0200

    Comments added to source code

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together makeCacheMatrix and cacheSolve make it possible to cache the inverse
+## of a matrix.
+## This functionality can be used in order to save the costly computation of
+## repeatedly calculating the inverse of a matrix.
+## The first time cacheSolve is called for a particular matrix it will calculate
+## its inverse as well as cache this value for futher use.
+## As long as the matrix does not change, this cached inverse will be recalled
+## when needed (i.e. no inverse re-calculation of the same matrix is done)
+## If the matrix changes or an inverse for a new matrix is required cacheSolve
+## will calculate the inverse for the new matrix and cache this new value for
+## further use.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a special vector that is able to cache the inverse
+## of a matrix.
+## Use $get() and $getinverse() to view the matrix and its inverse
+## Usage: 
+## x <- makeCacheMatrix(matrix(c(5,6,7,8),nrow=2))
 
 makeCacheMatrix <- function(x = matrix()) {
+             ## Create a vector that is able to cache the inverse of a matrix
 
     m <- NULL
     set <- function(y) {
@@ -20,7 +35,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will either compute the inverse of the matrix returned by
+## makeCacheMatrix or if available will retrieve the cached inverse of the matrix.
+## If the inverse has already been calculated and the matrix has not changed
+## then cacheSolve retrieves the inverse from the cache.
+## Usage:
+## cacheSolve(x)
 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
